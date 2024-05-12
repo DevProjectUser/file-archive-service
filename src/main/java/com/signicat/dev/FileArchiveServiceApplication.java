5a9b6b38ac1b23c63fc87f4042e8bc515b05e581
DevProjectUser <swarnimudemy@gmail.com>
2024-05-12 17:26:45 +0200

    Initial project structure commit to repo.

---
+++ src/main/java/com/signicat/dev/FileArchiveServiceApplication.java
@@ -0,0 +1,13 @@
+package com.signicat.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileArchiveServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileArchiveServiceApplication.class, args);
+    }
+
+}
